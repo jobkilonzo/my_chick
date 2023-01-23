2fdea2bfdc55e563591d47eb0ede5ae9afd9c1bd
RocqJones <jonesmbindyo@gmail.com>
2023-01-23 22:29:04 +0300

    logic update

--- app/src/main/java/com/example/mychick/kuroiler/KuroilerDao.kt
+++ app/src/main/java/com/example/mychick/kuroiler/KuroilerDao.kt
@@ -10,10 +10,8 @@ interface KuroilerDao {
     @Insert(onConflict =  OnConflictStrategy.REPLACE)
     suspend fun insert(kuroilerCost: KuroilerCost)
 
-   // @Query("UPDATE sum SET value = value + :value WHERE id = :id")
-    //suspend fun updateSum(id: Int, value: Long)
-    @Query("UPDATE kuroiler_cost SET amount = amount + :amount WHERE type = :type")
-    suspend fun updateCost(type: String, amount: Int)
+    @Query("UPDATE kuroiler_cost SET amount = :amount WHERE id = :id")
+    suspend fun updateCost(id: Int, amount: Int)
 
     @Query("SELECT * FROM kuroiler_cost")
     fun getAllCost(): Flow<List<KuroilerCost>>
@@ -23,11 +21,11 @@ interface KuroilerDao {
 
     @Query("SELECT amount FROM kuroiler_cost")
     fun geCost(): Flow<Int>
-    @Query("SELECT type FROM kuroiler_cost WHERE type = :type")
-    fun getSumById(type: String): String
+    @Query("SELECT id FROM kuroiler_cost WHERE id = :id")
+    fun getSumById(id: Int): String
 
-    @Transaction
-    suspend fun updateOrInsertCost(kuroilerCost: KuroilerCost) =
+//    @Transaction
+//    suspend fun updateOrInsertCost(kuroilerCost: KuroilerCost) =
         // <-- this method updates value or insert new item, if id is not found
-        updateCost(kuroilerCost.type, kuroilerCost.amount)
+        //updateCost(kuroilerCost.type, kuroilerCost.amount)
 }
--- app/src/main/java/com/example/mychick/kuroiler/KuroilerRepository.kt
+++ app/src/main/java/com/example/mychick/kuroiler/KuroilerRepository.kt
@@ -4,27 +4,22 @@ import androidx.annotation.WorkerThread
 import kotlinx.coroutines.flow.Flow
 
 class KuroilerRepository(private val kuroilerDao: KuroilerDao) {
+
     val allCost: Flow<List<KuroilerCost>> = kuroilerDao.getAllCost()
-
-    //val cost: Flow<Int> = kuroilerDao.geCost()
-
-
 
     suspend fun isEmpty(): Boolean = kuroilerDao.isEmpty()
 
-    fun getSumById(type: String){
-        kuroilerDao.getSumById(type)
+    fun getSumById(id: Int) {
+        kuroilerDao.getSumById(id)
     }
 
-   // val cost = kuroilerDao.updateOrInsertCost(KuroilerCost())
-   @WorkerThread
-   suspend fun updateCost(type: String, amount: Int){
-       kuroilerDao.updateCost(type, amount)
-   }
     @WorkerThread
-    suspend fun insert(kuroilerCost: KuroilerCost){
+    suspend fun updateCost(id: Int, amount: Int) {
+        kuroilerDao.updateCost(id, amount)
+    }
+
+    @WorkerThread
+    suspend fun insert(kuroilerCost: KuroilerCost) {
         kuroilerDao.insert(kuroilerCost)
     }
-
-
 }
