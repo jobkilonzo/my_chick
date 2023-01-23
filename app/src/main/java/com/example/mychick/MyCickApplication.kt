d3bc8dc8f5d7c656a23c907598245cb4ca9352ad
RocqJones <jonesmbindyo@gmail.com>
2023-01-23 22:28:32 +0300

    added CoroutineScope

--- app/src/main/java/com/example/mychick/MyCickApplication.kt
+++ app/src/main/java/com/example/mychick/MyCickApplication.kt
@@ -2,8 +2,13 @@ package com.example.mychick
 
 import android.app.Application
 import com.example.mychick.kuroiler.KuroilerRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class MyCickApplication : Application() {
-    val database by lazy { MyChickDatabase.getDabase(this) }
+
+    private val applicationScope = CoroutineScope(SupervisorJob())
+
+    private val database by lazy { MyChickDatabase.getDatabase(this, applicationScope) }
     val kuroilerRepository by lazy { KuroilerRepository(database.kuroilerDao()) }
 }
